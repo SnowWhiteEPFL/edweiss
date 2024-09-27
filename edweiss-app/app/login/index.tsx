cd03a8b5bf09066980ce0235554017ac95e05a57
Holo <yousseflaraki99@gmail.com>
2024-09-27 14:11:42 +0200

    Added temporary "login" screen

---
+++ edweiss-app/app/login/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const index = () => {
+	return (
+		<View>
+			<Text>I need to login</Text>
+		</View>
+	)
+}
+
+export default index
