3e48c73f8267a585224a15333ffd7720e16f3e57
Holo <yousseflaraki99@gmail.com>
2024-09-29 20:00:04 +0200

    Theme is now created at runtime depending on the phone theme

--- edweiss-app/app/_layout.tsx
+++ edweiss-app/app/_layout.tsx
@@ -7,42 +7,54 @@ import { useEffect } from 'react';
 import { AuthContextProvider } from '@/contexts/AuthContext';
 import * as SystemUI from 'expo-system-ui';
 
-const Light: Theme = {
-	dark: false,
-	colors: {
-		background: Colors.light.background,
-		border: Colors.light.background,
-		card: Colors.light.background,
-		notification: Colors.light.tint,
-		primary: Colors.light.tint,
-		text: Colors.light.text
-	}
-};
+// const Light: Theme = {
+// 	dark: false,
+// 	colors: {
+// 		background: Colors.light.mantle,
+// 		border: Colors.light.base,
+// 		card: Colors.light.base,
+// 		notification: Colors.light.blue,
+// 		primary: Colors.light.blue,
+// 		text: Colors.light.text
+// 	}
+// };
 
-const Dark: Theme = {
-	dark: true,
-	colors: {
-		background: Colors.dark.background,
-		border: Colors.dark.background,
-		card: Colors.dark.background,
-		notification: Colors.dark.tint,
-		primary: Colors.dark.tint,
-		text: Colors.dark.text,
-	}
-};
+// const Dark: Theme = {
+// 	dark: true,
+// 	colors: {
+// 		background: Colors.dark.mantle,
+// 		border: Colors.dark.base,
+// 		card: Colors.dark.base,
+// 		notification: Colors.dark.blue,
+// 		primary: Colors.dark.blue,
+// 		text: Colors.dark.text,
+// 	}
+// };
 
 export default function RootLayout() {
 	const theme = useTheme();
 
+	const ThemeObj: Theme = {
+		dark: theme == 'dark',
+		colors: {
+			background: Colors[theme].mantle,
+			border: Colors[theme].mantle,
+			card: Colors[theme].mantle,
+			notification: Colors[theme].blue,
+			primary: Colors[theme].blue,
+			text: Colors[theme].text,
+		}
+	}
+
 	useEffect(() => {
 		(async () => {
-			await SystemUI.setBackgroundColorAsync(Colors[theme].background);
+			await SystemUI.setBackgroundColorAsync(Colors[theme].mantle);
 		})();
 	}, [theme]);
 
 	return (
 		<AuthContextProvider>
-			<ThemeProvider value={theme === 'light' ? Light : Dark}>
+			<ThemeProvider value={ThemeObj}>
 				<Stack>
 					<Stack.Screen name="(app)" options={{ headerShown: false }} />
 					<Stack.Screen name="+not-found" />
