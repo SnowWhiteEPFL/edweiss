97750ce559ce00ea16a6d925fde29df134cd3a3c
Holo <yousseflaraki99@gmail.com>
2024-09-30 18:26:11 +0200

    add: common types utils

---
+++ edweiss-app/utils/types.ts
@@ -0,0 +1,9 @@
+
+export type Leaves<T> = T extends object ?
+	{ [K in keyof T]:
+		`${Exclude<K, symbol>}${Leaves<T[K]> extends never ? "" : `.${Leaves<T[K]>}`}`
+	}[keyof T] : never
+
+export function typeCheck<T>(t: T): T {
+	return t;
+}
