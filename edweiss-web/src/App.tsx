2459a0f1976cade43fbe57079bc1c13218574209
Holo <yousseflaraki99@gmail.com>
2024-09-30 23:39:24 +0200

    feat: add tailwindcss, nextui and file based routing

--- edweiss-web/src/App.tsx
+++ edweiss-web/src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css'
+import HeartIcon from '@heroicons/react/24/solid/HeartIcon'
+import { Button, Input } from '@nextui-org/react'
 
 function App() {
 	return (
-		<>
-			<span>EdWeiss Web app</span>
-		</>
+		<div>
+			<span className='text-blue-500'>EdWeiss Web app</span>
+			<Button color='primary'>
+				Hello
+			</Button>
+			<Input type="email" label="Email" />
+			<Button isIconOnly color='danger'>
+				<HeartIcon />
+			</Button>
+		</div>
 	)
 }
 
