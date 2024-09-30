e165965e444e4ed2c0a62f837a28ef7c2cfae6b8
Holo <yousseflaraki99@gmail.com>
2024-09-30 20:00:56 +0200

    refactor: renamed edweiss-model to model

--- model/update.sh
+++ model/update.sh
@@ -4,17 +4,16 @@ NC='\033[0m'
 BOLD=$(tput bold)
 
 chmod +w ../edweiss-app/model/*
-cp -r "model" "../edweiss-app/"
+cp -r "src/." "../edweiss-app/model/"
 chmod -w ../edweiss-app/model/*
 printf "${GREEN} \xE2\x9C\x94 ${NC} ${BOLD} Model updated in application.\n"
 
 chmod +w ../edweiss-web/src/model/*
-cp -r "model" "../edweiss-web/src/"
+cp -r "src/." "../edweiss-web/src/model/"
 chmod -w ../edweiss-web/src/model/*
 printf "${GREEN} \xE2\x9C\x94 ${NC} ${BOLD} Model updated in web application.\n"
 
 chmod +w ../edweiss-firebase/functions/src/model/*
-cp -r "model" "../edweiss-firebase/functions/src/"
+cp -r "src/." "../edweiss-firebase/functions/src/model/"
 chmod -w ../edweiss-firebase/functions/src/model/*
 printf "${GREEN} \xE2\x9C\x94 ${NC} ${BOLD} Model updated in cloud functions.\n\n"
-
