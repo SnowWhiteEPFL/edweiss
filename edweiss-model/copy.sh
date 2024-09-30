c50bc37ddedc850ff152a7156356fdd70302f649
Holo <yousseflaraki99@gmail.com>
2024-09-30 15:19:36 +0200

    fix: updating the model outputs a prettier result

--- edweiss-model/copy.sh
+++ edweiss-model/copy.sh
@@ -1,10 +1,15 @@
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+BOLD=$(tput bold)
+
 chmod +w ../edweiss-app/model/*
 cp -r "model" "../edweiss-app/"
 chmod -w ../edweiss-app/model/*
-echo "Model updated in application."
+printf "${GREEN} \xE2\x9C\x94 ${NC} ${BOLD} Model updated in application.\n"
 
 chmod +w ../edweiss-firebase/functions/src/model/*
 cp -r "model" "../edweiss-firebase/functions/src/"
 chmod -w ../edweiss-firebase/functions/src/model/*
-echo "Model updated in cloud functions."
+
+printf "${GREEN} \xE2\x9C\x94 ${NC} ${BOLD} Model updated in cloud functions.\n\n"
