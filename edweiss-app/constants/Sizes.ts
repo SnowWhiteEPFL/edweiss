ffdd2412a993d15d60359a26a2fc3c4672bce79c
Holo <yousseflaraki99@gmail.com>
2024-10-02 20:54:03 +0200

    refactor: amplifies a bit the default padding sizes

--- edweiss-app/constants/Sizes.ts
+++ edweiss-app/constants/Sizes.ts
@@ -40,11 +40,11 @@ const marginSizes: PredefinedSizes = {
 }
 
 const paddingSizes: PredefinedSizes = {
-	xs: 2,
-	sm: 4,
-	md: 8,
-	lg: 12,
-	xl: 16
+	xs: 4,
+	sm: 8,
+	md: 16,
+	lg: 24,
+	xl: 32
 }
 
 const borderSizes: PredefinedSizes = {
