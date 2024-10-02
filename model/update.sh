76c223973c804466091a1f3434b549c4bc25a0db
Holo <yousseflaraki99@gmail.com>
2024-10-02 21:46:02 +0200

    feat: filters the inodes affected by the writing restriction (only files)

--- model/update.sh
+++ model/update.sh
@@ -3,17 +3,19 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 BOLD=$(tput bold)
 
-chmod +w ../edweiss-app/model/*
+rm -rf ../edweiss-app/model/
 cp -r "src/." "../edweiss-app/model/"
-chmod -w ../edweiss-app/model/*
+find ../edweiss-app/model/ -type f -exec bash -c 'chmod -w "{}"' \;
 printf "${GREEN} \xE2\x9C\x94 ${NC} ${BOLD} Model updated in application.\n"
 
-chmod +w ../edweiss-web/src/model/*
+rm -rf ../edweiss-web/src/model/
 cp -r "src/." "../edweiss-web/src/model/"
-chmod -w ../edweiss-web/src/model/*
+find ../edweiss-web/src/model/ -type f -exec bash -c 'chmod -w "{}"' \;
 printf "${GREEN} \xE2\x9C\x94 ${NC} ${BOLD} Model updated in web application.\n"
 
-chmod +w ../edweiss-firebase/functions/src/model/*
+rm -rf ../edweiss-firebase/functions/src/model/
 cp -r "src/." "../edweiss-firebase/functions/src/model/"
-chmod -w ../edweiss-firebase/functions/src/model/*
-printf "${GREEN} \xE2\x9C\x94 ${NC} ${BOLD} Model updated in cloud functions.\n\n"
+find ../edweiss-firebase/functions/src/model/ -type f -exec bash -c 'chmod -w "{}"' \;
+printf "${GREEN} \xE2\x9C\x94 ${NC} ${BOLD} Model updated in cloud functions.\n"
+
+printf "\n"
