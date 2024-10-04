f902e6465db5d3a6cdec3cd45e53c0daed06db62
Holo <yousseflaraki99@gmail.com>
2024-10-04 16:43:51 +0200

    feat: adds basic react page snippet

--- edweiss-app/constants/Component.ts
+++ edweiss-app/constants/Component.ts
@@ -3,4 +3,8 @@ import React from 'react';
 
 type ReactComponent<P> = React.FC<Readonly<P>>
 
+export type ReactPage = React.FC<{}>
+
+export type Setter<T> = React.Dispatch<React.SetStateAction<T>>
+
 export default ReactComponent
