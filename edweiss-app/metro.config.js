2b1d0847841c85229ccc91441e10b9efef381cd4
Holo <yousseflaraki99@gmail.com>
2024-10-07 13:14:43 +0200

    feat: adds support for svg files

--- edweiss-app/metro.config.js
+++ edweiss-app/metro.config.js
@@ -1,2 +1,25 @@
 require('ts-node/register');
-module.exports = require('./metro.config.ts');
+
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+	const config = getDefaultConfig(__dirname);
+
+	config.resolver.sourceExts.push('cjs');
+
+	const { transformer, resolver } = config;
+
+	config.transformer = {
+		...transformer,
+		babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+	};
+	config.resolver = {
+		...resolver,
+		assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+		sourceExts: [...resolver.sourceExts, "svg"]
+	};
+
+	return config;
+})();
+
+// module.exports = require('./metro.config.ts');
