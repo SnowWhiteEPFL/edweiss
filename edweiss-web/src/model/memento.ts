7e8d9bbabbde78809d30ab97881da327f17c4fa3
Tuan <tuan.nguyen@epfl.ch>
2024-10-08 22:29:23 +0200

    feat: model for create deck, delete deck, create card and delete card

--- edweiss-web/src/model/memento.ts
+++ edweiss-web/src/model/memento.ts
@@ -13,16 +13,43 @@ namespace Memento {
 	export interface Card {
 		question: CardQuestion,
 		answer: string;
+		learning_status: LearningStatus;
 	}
+
+	export type LearningStatus = "Got it" | "Not yet" | undefined;
 
 	export interface Deck {
 		name: string,
 		cards: Card[];
 	}
 
-	export const Functions = FunctionFolder("memento", {
+	export const createDeck = FunctionFolder("memento", {
 		creation: FunctionFolder("creation", {
 			createDeck: FunctionOf<{ deck: Memento.Deck; }, { id: string; }, 'empty_deck'>("createDeck"),
+		})
+	});
+
+	export const deleteDeck = FunctionFolder("memento", {
+		deletion: FunctionFolder("deletion", {
+			deleteDeck: FunctionOf<{ deckId: string; }, { id: string; }, 'deck_not_found'>("deleteDeck"),
+		})
+	});
+
+	export const createCard = FunctionFolder("memento", {
+		creation: FunctionFolder("creation", {
+			createCard: FunctionOf<{ deckId: any; card: Memento.Card; }, { id: string; }, 'deck_not_found'>("createCard"),
+		})
+	});
+
+	export const deleteCard = FunctionFolder("memento", {
+		deletion: FunctionFolder("deletion", {
+			deleteCard: FunctionOf<{ deckId: string; cardId: string; }, { id: string; }, 'deck_not_found'>("deleteCard"),
+		})
+	});
+
+	export const updateDeck = FunctionFolder("memento", {
+		update: FunctionFolder("update", {
+			updateDeck: FunctionOf<{ deckId: any; card: Memento.Card; }, { id: string; }, 'deck_not_found'>("updateDeck"),
 		})
 	});
 }
