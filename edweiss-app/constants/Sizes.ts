9cc77c1cc18bb9f52618c65031c409bc502b771c
Holo <yousseflaraki99@gmail.com>
2024-10-08 16:08:46 +0200

    refactor: changes horizontal and vertical sizes to x and y (tailwind style)

--- edweiss-app/constants/Sizes.ts
+++ edweiss-app/constants/Sizes.ts
@@ -11,8 +11,8 @@ export interface PaddingProps {
 	pb?: Size,
 	pl?: Size,
 	pr?: Size,
-	ph?: Size,
-	pv?: Size;
+	px?: Size,
+	py?: Size;
 }
 
 export interface MarginProps {
@@ -21,8 +21,8 @@ export interface MarginProps {
 	mb?: Size,
 	ml?: Size,
 	mr?: Size,
-	mh?: Size,
-	mv?: Size;
+	mx?: Size,
+	my?: Size;
 }
 
 export interface BorderProps {
@@ -31,8 +31,8 @@ export interface BorderProps {
 	bb?: Size,
 	bl?: Size,
 	br?: Size,
-	bh?: Size,
-	bv?: Size;
+	bx?: Size,
+	by?: Size;
 }
 
 export type BoxModelProps = PaddingProps & MarginProps & BorderProps;
@@ -107,43 +107,43 @@ export function computeSizeOpt(size: Size | undefined, sizes: PredefinedSizes): 
 
 export function computePaddings(paddings: PaddingProps) {
 	const p = computeSizeOpt(paddings.p, paddingSizes);
-	const ph = computeSizeOpt(paddings.ph, paddingSizes);
-	const pv = computeSizeOpt(paddings.pv, paddingSizes);
+	const px = computeSizeOpt(paddings.px, paddingSizes);
+	const py = computeSizeOpt(paddings.py, paddingSizes);
 
 	return {
 		padding: p,
-		paddingTop: paddings.pt == undefined ? (pv ?? p) : computeSizeOpt(paddings.pt, paddingSizes),
-		paddingBottom: paddings.pb == undefined ? (pv ?? p) : computeSizeOpt(paddings.pb, paddingSizes),
-		paddingLeft: paddings.pl == undefined ? (ph ?? p) : computeSizeOpt(paddings.pl, paddingSizes),
-		paddingRight: paddings.pr == undefined ? (ph ?? p) : computeSizeOpt(paddings.pr, paddingSizes),
+		paddingTop: paddings.pt == undefined ? (py ?? p) : computeSizeOpt(paddings.pt, paddingSizes),
+		paddingBottom: paddings.pb == undefined ? (py ?? p) : computeSizeOpt(paddings.pb, paddingSizes),
+		paddingLeft: paddings.pl == undefined ? (px ?? p) : computeSizeOpt(paddings.pl, paddingSizes),
+		paddingRight: paddings.pr == undefined ? (px ?? p) : computeSizeOpt(paddings.pr, paddingSizes),
 	};
 }
 
 export function computeMargins(margins: MarginProps) {
 	const m = computeSizeOpt(margins.m, marginSizes);
-	const mh = computeSizeOpt(margins.mh, marginSizes);
-	const mv = computeSizeOpt(margins.mv, marginSizes);
+	const mx = computeSizeOpt(margins.mx, marginSizes);
+	const my = computeSizeOpt(margins.my, marginSizes);
 
 	return {
 		margin: m,
-		marginTop: margins.mt == undefined ? (mv ?? m) : computeSizeOpt(margins.mt, marginSizes),
-		marginBottom: margins.mb == undefined ? (mv ?? m) : computeSizeOpt(margins.mb, marginSizes),
-		marginLeft: margins.ml == undefined ? (mh ?? m) : computeSizeOpt(margins.ml, marginSizes),
-		marginRight: margins.mr == undefined ? (mh ?? m) : computeSizeOpt(margins.mr, marginSizes),
+		marginTop: margins.mt == undefined ? (my ?? m) : computeSizeOpt(margins.mt, marginSizes),
+		marginBottom: margins.mb == undefined ? (my ?? m) : computeSizeOpt(margins.mb, marginSizes),
+		marginLeft: margins.ml == undefined ? (mx ?? m) : computeSizeOpt(margins.ml, marginSizes),
+		marginRight: margins.mr == undefined ? (mx ?? m) : computeSizeOpt(margins.mr, marginSizes),
 	};
 }
 
 export function computeBorders(borders: BorderProps) {
 	const b = computeSizeOpt(borders.b, borderSizes);
-	const bh = computeSizeOpt(borders.bh, borderSizes);
-	const bv = computeSizeOpt(borders.bv, borderSizes);
+	const bx = computeSizeOpt(borders.bx, borderSizes);
+	const by = computeSizeOpt(borders.by, borderSizes);
 
 	return {
 		borderWidth: b,
-		borderTopWidth: borders.bt == undefined ? (bv ?? b) : computeSizeOpt(borders.bt, borderSizes),
-		borderBottomWidth: borders.bb == undefined ? (bv ?? b) : computeSizeOpt(borders.bb, borderSizes),
-		borderLeftWidth: borders.bl == undefined ? (bh ?? b) : computeSizeOpt(borders.bl, borderSizes),
-		borderRightWidth: borders.br == undefined ? (bh ?? b) : computeSizeOpt(borders.br, borderSizes),
+		borderTopWidth: borders.bt == undefined ? (by ?? b) : computeSizeOpt(borders.bt, borderSizes),
+		borderBottomWidth: borders.bb == undefined ? (by ?? b) : computeSizeOpt(borders.bb, borderSizes),
+		borderLeftWidth: borders.bl == undefined ? (bx ?? b) : computeSizeOpt(borders.bl, borderSizes),
+		borderRightWidth: borders.br == undefined ? (bx ?? b) : computeSizeOpt(borders.br, borderSizes),
 	};
 }
 
