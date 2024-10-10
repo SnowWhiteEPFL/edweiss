73940db2dde0d0367ed55eba6ad0b439da0209fc
Gustavo <gutogmaia@gmail.com>
2024-10-10 18:45:01 +0200

    feat: access firebase cloud storage for pdf import, pdf display functionalities improved, templates for speech-to-text and question chatbox

--- edweiss-app/app/(app)/lectures/slides/index.tsx
+++ edweiss-app/app/(app)/lectures/slides/index.tsx
@@ -1,15 +1,21 @@
+import TScrollView from '@/components/core/containers/TScrollView';
+import TTouchableOpacity from '@/components/core/containers/TTouchableOpacity';
+import TView from '@/components/core/containers/TView';
 import RouteHeader from '@/components/core/header/RouteHeader';
+import Icon from '@/components/core/Icon';
+import TText from '@/components/core/TText';
+import FancyTextInput from '@/components/input/FancyTextInput';
+import { getDownloadURL } from '@/config/firebase';
 import { ApplicationRoute } from '@/constants/Component';
-import SlidesDisplay from '@/model/lectures/slides';
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, Dimensions, Linking, View } from 'react-native';
+import { ActivityIndicator, Dimensions, Linking } from 'react-native';
 import Pdf from 'react-native-pdf';
 
 const Slide: ApplicationRoute = () => {
-    const [numPages, setNumPages] = useState<number>(10);  // Number of pages in the PDF
+    const [numPages, setNumPages] = useState<number>(0);  // Number of pages in the PDF
     const [page, setPage] = useState<number>(1);  // Current page, starting from 1
-    const [loading, setLoading] = useState<boolean>(true);  // Loading state
-    const [images, setImages] = useState<SlidesDisplay.Slides>();  // Loading state
+    //const [images, setImages] = useState<SlidesDisplay.Slides>();  // Loading state
+    const [url, setUrl] = useState<string>('');  // Loading state
 
     // Function to go to the next page
     function pageForward() {
@@ -21,6 +27,12 @@ const Slide: ApplicationRoute = () => {
         if (page > 1) setPage(page - 1);
     }
 
+    // Funtion to set Url to the desired one from firebase storage
+    async function getUrl() {
+        setUrl(await getDownloadURL('China-101.pdf'));
+    }
+
+    /*
     function loadPdf() {
         const imgList: SlidesDisplay.PdfPage[] = [];
         let count = 0;
@@ -28,72 +40,56 @@ const Slide: ApplicationRoute = () => {
         setImages({ images: imgList });
         console.log(images?.images[0].path);
         setLoading(false);
-    }
+    }*/
 
     // Load images when the component mounts
     useEffect(() => {
-        loadPdf();
+        getUrl();
     }, []);
 
     return (
 
         <>
             <RouteHeader title={"Lecture's Slides"} />
-            <View style={{
-                flex: 1,
-                justifyContent: "flex-start",
-                alignItems: "center"
-            }}>
+            <TView mb={'xl'} flexDirection='row'>
+
+                <TView flexDirection='column' justifyContent='space-between'>
+                    <TTouchableOpacity backgroundColor='base' onPress={pageBack}>
+                        <Icon size={'xl'} name={'arrow-up-circle-outline'} dark='text'></Icon>
+                    </TTouchableOpacity>
+
+                    <TTouchableOpacity backgroundColor='base' onPress={pageForward}>
+                        <Icon size={'xl'} name={'arrow-down-circle-outline'} dark='text'></Icon>
+                    </TTouchableOpacity>
+                </TView>
+
                 <Pdf
                     trustAllCerts={false}
-                    source={{ uri: images?.images[0].path }}
+                    source={{ uri: url }}
                     spacing={1}
                     renderActivityIndicator={() =>
                         <ActivityIndicator size={'large'} />
                     }
                     enablePaging={true}
-                    onLoadProgress={(percentage) => console.log(`Loading : ${percentage}`)}
-                    onLoadComplete={() => console.log("Loading Complete")}
-                    onPageChanged={(page, totalPages) => console.log(`${page}/${totalPages}`)}
+                    //onLoadProgress={(percentage) => console.log(`Loading : ${percentage}`)}
+                    //onLoadComplete={() => console.log("Loading Complete")}
+                    onPageChanged={(page, totalPages) => setNumPages(totalPages)}
                     onError={(error) => console.log(error)}
-                    onPageSingleTap={(page) => console.log(page)}
+                    //onPageSingleTap={(page) => console.log(page)}
                     onPressLink={(link) => Linking.openURL(link)}
                     page={page}
-                    //horizontal
-                    style={{ flex: 1, width: Dimensions.get('window').width }} />
-            </View>
+                    style={{ flex: 1, width: Dimensions.get('window').width * 0.8, height: Dimensions.get('window').width }} />
 
-
-
+            </TView>
+            <TView flexDirection='row' style={{ height: "100%" }}>
+                <TScrollView flex={0.7} b={'sm'} mr={'md'}>
+                    <FancyTextInput label='Ask your questions' mt={'xl'} icon='chatbubbles-outline'></FancyTextInput>
+                </TScrollView>
+                <TScrollView b={'sm'} >
+                    <TText> Speech to Text here</TText>
+                </TScrollView>
+            </TView>
         </>
-        //<RouteHeader title={"Lecture's Slides"} />
-
-
-        //<TView>
-        //    {loading ? (
-        //        <TActivityIndicator />  // Show loading indicator while the PDF loads
-        //    ) : (
-        //<TView mb={'md'} mr={'xl'} flexDirection='row' justifyContent='flex-start'>
-        //    <FancyButton icon='arrow-back-circle-outline' onPress={pageBack} />
-
-        //    <Pdf
-        //trustAllCerts={false}
-        //        source={{ uri: 'file:///C:/Users/rdcma/Downloads/A%20leap%20into%20the%20past_%20Using%20Usenet.pdf' }}  // PDF file source
-        // onLoadComplete={(numberOfPages) => {
-        //    setNumPages(numberOfPages);
-        //    setLoading(false);  // Stop loading once PDF is loaded
-        //}}
-        //onPageChanged={(page, numberOfPages) => {
-        //    console.log(`Current page: ${page} / ${numberOfPages}`);
-        //}}
-        //page={page}  // Display the current page
-        //    />
-
-        //    <FancyButton icon='arrow-forward-circle-outline' onPress={pageForward} />
-        //</TView>
-        //    )}
-        //</TView>
-        //</>
     );
 };
 
