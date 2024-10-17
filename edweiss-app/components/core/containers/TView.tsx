9eb3508e300e7421b1d9dd6eeb9c5f477fdc5dec
Tuan <tuan.nguyen@epfl.ch>
2024-10-17 20:22:59 +0200

    chore: modify color to adapt to the test. This must be dicussed and fixed later with Youssef

--- edweiss-app/components/core/containers/TView.tsx
+++ edweiss-app/components/core/containers/TView.tsx
@@ -1,14 +1,15 @@
 import ReactComponent from '@/constants/Component';
 
 import { ContainerProps, computeContainerStyle } from '@/constants/Style';
-import { useOptionalColor, useOptionalThemeColor } from '@/hooks/theme/useThemeColor';
 import { View, type ViewProps } from 'react-native';
 
 export type TViewProps = ViewProps & ContainerProps;
 
 const TView: ReactComponent<TViewProps> = ({ backgroundColor, borderColor, style, columnGap = 0, rowGap = 0, ...props }) => {
-	const computedBackgroundColor = useOptionalThemeColor({ light: props.light, dark: props.dark }, backgroundColor);
-	const computedBorderColor = useOptionalColor(borderColor);
+	//const computedBackgroundColor = useOptionalThemeColor({ light: props.light, dark: props.dark }, backgroundColor);
+	//const computedBorderColor = useOptionalColor(borderColor);
+	const computedBackgroundColor = backgroundColor;
+	const computedBorderColor = borderColor;
 
 	return <View
 		style={
