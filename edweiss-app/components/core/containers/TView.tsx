146de8457a66dcff15832dd6dda0e047242c0b82
Adamm Alaoui <adamm.2.alaoui@gmail.com>
2024-10-18 09:12:35 +0200

    merge: forgotten to apply the row column gap flex issue solved before

--- edweiss-app/components/core/containers/TView.tsx
+++ edweiss-app/components/core/containers/TView.tsx
@@ -5,9 +5,9 @@ import { View, type ViewProps } from 'react-native';
 
 export type TViewProps = ViewProps & ContainerProps;
 
-const TView: ReactComponent<TViewProps> = ({ backgroundColor, borderColor, style, columnGap = 0, rowGap = 0, ...props }) => {
-	//const computedBackgroundColor = useOptionalThemeColor({ light: props.light, dark: props.dark }, backgroundColor);
-	//const computedBorderColor = useOptionalColor(borderColor);
+const TView: ReactComponent<TViewProps> = ({ backgroundColor, borderColor, style, ...props }) => {
+	// const computedBackgroundColor = useOptionalThemeColor({ light: props.light, dark: props.dark }, backgroundColor);
+	// const computedBorderColor = useOptionalColor(borderColor);
 	const computedBackgroundColor = backgroundColor;
 	const computedBorderColor = borderColor;
 
