859506d1f2098dba55261c36a831a5f65e6c1fc4
Holo <yousseflaraki99@gmail.com>
2024-10-31 17:00:30 +0100

    feat: repurposed the explore tab into a community tab

--- edweiss-app/app/(app)/(tabs)/community.tsx
+++ edweiss-app/app/(app)/(tabs)/community.tsx
@@ -4,20 +4,16 @@ import RouteHeader from '@/components/core/header/RouteHeader';
 import FancyButton from '@/components/input/FancyButton';
 import { ApplicationRoute } from '@/constants/Component';
 import { router } from 'expo-router';
-import { useState } from 'react';
 
 
-const ExploreTab: ApplicationRoute = () => {
-	const [pageCount, setPageCount] = useState<number>(1);
-
-
+const CommunityTab: ApplicationRoute = () => {
 	return (
 		<>
-			<RouteHeader title={"Explore"} />
+			<RouteHeader title={"Community"} />
 
 			<TView>
 				<TText>
-					Explore and experiment in explore.tsx !
+					Explore and experiment incommunity.tsx !
 				</TText>
 			</TView>
 
@@ -33,4 +29,4 @@ const ExploreTab: ApplicationRoute = () => {
 	);
 };
 
-export default ExploreTab;
+export default CommunityTab;
