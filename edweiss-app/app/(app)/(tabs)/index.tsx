dec5fd28def30c0861120c79030328b5c352a09d
Holo <yousseflaraki99@gmail.com>
2024-10-31 17:01:34 +0100

    feat: adds a notification button

--- edweiss-app/app/(app)/(tabs)/index.tsx
+++ edweiss-app/app/(app)/(tabs)/index.tsx
@@ -3,11 +3,15 @@ import TScrollView from '@/components/core/containers/TScrollView';
 import TTouchableOpacity from '@/components/core/containers/TTouchableOpacity';
 import TView from '@/components/core/containers/TView';
 import For from '@/components/core/For';
+import HeaderButton from '@/components/core/header/HeaderButton';
+import HeaderButtons from '@/components/core/header/HeaderButtons';
+import RouteHeader from '@/components/core/header/RouteHeader';
 import Icon from '@/components/core/Icon';
 import TActivityIndicator from '@/components/core/TActivityIndicator';
 import TText from '@/components/core/TText';
 import { CollectionOf, Document } from '@/config/firebase';
-import { Color } from '@/constants/Colors';
+import t from '@/config/i18config';
+import { Color, courseColors } from '@/constants/Colors';
 import ReactComponent, { ApplicationRoute } from '@/constants/Component';
 import { useCourses } from '@/contexts/courses';
 import { useDocs } from '@/hooks/firebase/firestore';
@@ -23,22 +27,30 @@ const HomeTab: ApplicationRoute = () => {
 		return <TActivityIndicator size={40} />
 
 	return (
-		<TScrollView showsVerticalScrollIndicator={true}>
-			{courses.length == 0 && <TText>Debug: You have no course registered on Firebase.</TText>}
+		<>
+			<RouteHeader title='Home' right={
+				<HeaderButtons>
+					<HeaderButton onPress={() => router.push("/notifications" as any)} icon='notifications-outline' />
+				</HeaderButtons>
+			} />
 
-			<Schedule courses={courses} />
+			<TScrollView showsVerticalScrollIndicator={true} pt={'sm'}>
+				{courses.length == 0 && <TText>Debug: You have no course registered on Firebase.</TText>}
 
-			<TView flexDirection='row' mx={"md"} alignItems='center' flexColumnGap={8} mb={"sm"}>
-				<Icon name='school-outline' size={18} />
-				<TText>
-					Courses
-				</TText>
-			</TView>
+				<Schedule courses={courses} />
 
-			<For each={courses}>
-				{course => <CourseDisplay key={course.id} course={course} />}
-			</For>
-		</TScrollView>
+				<TView flexDirection='row' mx={"md"} alignItems='center' flexColumnGap={8} mb={"sm"}>
+					<Icon name='school-outline' size={18} />
+					<TText>
+						Courses
+					</TText>
+				</TView>
+
+				<For each={courses}>
+					{course => <CourseDisplay key={course.id} course={course} />}
+				</For>
+			</TScrollView>
+		</>
 	);
 };
 
@@ -50,14 +62,21 @@ const Schedule: ReactComponent<{ courses: Document<Course>[] }> = ({ courses }) 
 			<TView flexDirection='row' alignItems='center' flexColumnGap={8} mb={'xs'}>
 				<Icon name='calendar-outline' size={18} />
 				<TText>
-					Schedule
+					{t("home:schedule")}
 				</TText>
 			</TView>
 
-			<SchedulePoint color='green' time='mar. 08:15' name='Responsible Software' room='CO3' />
-			<SchedulePoint color='blue' time='mar. 10:15' name='Quantum Mechanics for Non-Physicists' room='ELE20' />
+			{
+				/**
+				 * Schedule point should
+				 */
+			}
 
-			{/* <TText align='center' color='blue' my={'md'}>Go to calendar</TText> */}
+			<SchedulePoint color={courseColors["lab"]} time='tue. 08:15' name='Responsible Software' room='CO3' />
+			<SchedulePoint color={courseColors["lecture"]} time='tue. 10:15' name='Quantum Mechanics for Non-Physicists' room='INF100' />
+			<SchedulePoint color={courseColors["project"]} time='wed. 13:15' name='Software Entreprise' room='ELE20' />
+
+			{/* <TText align='center' color='subtext0' size={'sm'} mt={'sm'}>See more</TText> */}
 		</TTouchableOpacity>
 	);
 };
@@ -88,12 +107,15 @@ const CourseDisplay: ReactComponent<{ course: Document<Course> }> = ({ course })
 
 	return (
 		<TTouchableOpacity onPress={() => router.push(`/courses/${course.id}`)} radius={'md'} backgroundColor='base' mx={'md'} p={'md'} mb={"md"}>
-			<TText size={'lg'}>
+			<TText>
 				{course.data.name}
 			</TText>
 			<For each={assignments}>
 				{assignment => <AssignmentDisplay key={assignment.id} assignment={assignment} />}
 			</For>
+			<TText size={'sm'} color='overlay1'>
+				{t("home:no_assignments")}
+			</TText>
 		</TTouchableOpacity>
 		// <TTouchableOpacity onPress={() => router.push(`/courses/${course.id}`)} radius={'md'} flexDirection='row' p={5} borderColor='overlay2' backgroundColor='surface0' mb={8} >
 		// 	<TView flexDirection='column'>
