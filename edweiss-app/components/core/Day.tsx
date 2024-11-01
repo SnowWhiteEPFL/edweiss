6c35f65135e873ec5bc7fa781e72c44e8c21477d
Gwenaëlle <116882041+Gwen678@users.noreply.github.com>
2024-11-01 10:28:34 +0100

    improve maintability in Day.tsx

--- edweiss-app/components/core/Day.tsx
+++ edweiss-app/components/core/Day.tsx
@@ -10,7 +10,13 @@ export const Day = ({ period, course, user, filteredPeriods, index, format }: {
     period: CourseTimePeriod; course: { id: string; data: Course; }; user: any; filteredPeriods: any[]; index: number; format: string;
 }) => {
     const periodHeight = period.end ? ((period.end - period.start) / 60) * HOUR_BLOCK_HEIGHT : HOUR_BLOCK_HEIGHT;
-
+    const pathname = user.data.type == 'professor' ? '/(app)/startCourseScreen' : '/(app)/lectures/slides';
+    const params = user.data.type == 'professor' ? {
+        courseID: course.id, course: JSON.stringify(course.data), period: JSON.stringify(period), index,
+    } : {
+        courseNameString: course.data.name,
+        lectureIdString: period.activityId
+    };
     return (
 
         <TTouchableOpacity
@@ -22,22 +28,11 @@ export const Day = ({ period, course, user, filteredPeriods, index, format }: {
 
             onPress={() => {
                 {
-                    user.data.type == 'professor' && router.push({
-                        pathname: '/(app)/startCourseScreen',
-                        params: {
-                            courseID: course.id, course: JSON.stringify(course.data), period: JSON.stringify(period), index,
-                        }
+                    router.push({
+                        pathname: pathname,
+                        params: params
                     });
                 }
-                {
-                    user.data.type == 'student' && course.data.started && period.type == 'lecture' && router.push({
-                        pathname: '/(app)/lectures/slides' as any,
-                        params: {
-                            courseNameString: course.data.name,
-                            lectureIdString: period.activityId
-                        }
-                    });
-                };
             }}
             backgroundColor={courseColors[period.type as keyof typeof courseColors] as Color || 'base'}
             style={{
