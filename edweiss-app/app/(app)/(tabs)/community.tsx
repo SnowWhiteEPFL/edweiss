562a3066e606a055a021dd8b9de6fbff253fd15f
Holo <yousseflaraki99@gmail.com>
2024-11-06 16:28:25 +0100

    chore: adds experiment button on the community tab to interact with the AI model

--- edweiss-app/app/(app)/(tabs)/community.tsx
+++ edweiss-app/app/(app)/(tabs)/community.tsx
@@ -2,11 +2,35 @@ import TText from '@/components/core/TText';
 import TView from '@/components/core/containers/TView';
 import RouteHeader from '@/components/core/header/RouteHeader';
 import FancyButton from '@/components/input/FancyButton';
+import { callFunction } from '@/config/firebase';
 import { ApplicationRoute } from '@/constants/Component';
+import Experiments from '@/model/experiments';
 import { router } from 'expo-router';
+import { useState } from 'react';
 
 
 const CommunityTab: ApplicationRoute = () => {
+	const [response, setResponse] = useState("");
+
+	async function generateAIResponse() {
+		setResponse("Calling function...");
+
+		try {
+			const res = await callFunction(Experiments.functions.promptAI, {
+				task: "This is a software engineering class. Correct the following lecture audio transcript.",
+				content: "Helwo eveyrone wlecome to today's lecture, anyways to day we will see New content."
+			});
+
+			if (res.status == 1) {
+				setResponse(res.data);
+			} else {
+				setResponse(res.error);
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
 	return (
 		<>
 			<RouteHeader title={"Community"} />
@@ -25,6 +49,13 @@ const CommunityTab: ApplicationRoute = () => {
 				My Todos
 			</FancyButton>
 
+			<FancyButton mt={'md'} mb={'md'} onPress={generateAIResponse}>
+				Generate AI response
+			</FancyButton>
+
+			<TText>
+				{JSON.stringify(response)}
+			</TText>
 		</>
 	);
 };
