92afc3ed5beebc24a64e40bf00a4874d4f462b27
Holo <yousseflaraki99@gmail.com>
2024-11-06 16:49:20 +0100

    feat: adds a more furnished prompt

--- edweiss-firebase/functions/src/functions/lectures/addAudioTranscript.ts
+++ edweiss-firebase/functions/src/functions/lectures/addAudioTranscript.ts
@@ -10,8 +10,9 @@
 
 import prompt from 'actions/ai';
 import LectureDisplay from 'model/lectures/lectureDoc';
+import { Course } from 'model/school/courses';
 import { onAuthentifiedCall } from 'utils/firebase';
-import { CollectionOf, getDocumentAndRef } from 'utils/firestore';
+import { CollectionOf, getDocumentAndRef, getRequiredDocument } from 'utils/firestore';
 import { fail, ok } from 'utils/status';
 import Functions = LectureDisplay.Functions;
 
@@ -27,7 +28,7 @@ export const addAudioTranscript = onAuthentifiedCall(Functions.addAudioTranscrip
 		return fail('invalid_arg');
 	}
 
-
+	const course = await getRequiredDocument(CollectionOf<Course>(`courses`), args.courseId, fail("course_not_found"));
 	const [lecture, lectureRef] = await getDocumentAndRef(CollectionOf<Lecture>(`courses/${args.courseId}/lectures`), args.lectureId);
 
 	if (lecture == undefined)
@@ -40,7 +41,12 @@ export const addAudioTranscript = onAuthentifiedCall(Functions.addAudioTranscrip
 	const pageKey = `audioTranscript.${args.pageNumber}`;
 
 	const correctedTranscript = await prompt({
-		task: "Correct the following audio transcript",
+		task: `
+			This is a "${course.name}" lecture at prestigious university EPFL.
+			Description of the course: ${course.description}.
+
+			You are an audiotranscript correcting AI. Correct the following text captured by the microphone during the lecture.
+		`,
 		content: args.transcription,
 		fallback: args.transcription
 	});
