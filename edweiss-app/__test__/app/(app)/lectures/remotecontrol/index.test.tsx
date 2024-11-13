4a9c66a73e885e0423d416bb0f08a46fe015aaf4
Adamm Alaoui <adamm.2.alaoui@gmail.com>
2024-11-13 10:30:40 +0100

    feat(test): add test to check the voice onfailure event

--- edweiss-app/__test__/app/(app)/lectures/remotecontrol/index.test.tsx
+++ edweiss-app/__test__/app/(app)/lectures/remotecontrol/index.test.tsx
@@ -20,21 +20,24 @@ import React, { ReactNode } from 'react';
 // -----------------  Mocking dependencies    -----------------
 // ------------------------------------------------------------
 
+// `t` to return the key as the translation
 jest.mock('@/config/i18config', () => ({
     __esModule: true,
     default: jest.fn((key) => key),
 }));
 
+// Expo router
 jest.mock('expo-router', () => ({
     router: { push: jest.fn() },
     Stack: {
         Screen: jest.fn(({ options }) => (
-            <>{options.title}</> // Simulate rendering the title for the test
+            <>{options.title}</>
         )),
     },
     useLocalSearchParams: jest.fn(() => ({ courseNameString: 'testCourse', lectureIdString: 'testLectureId' }))
 }));
 
+// Application navigation
 jest.mock('@react-navigation/native-stack', () => {
     const actualNav = jest.requireActual('@react-navigation/native-stack');
     return {
@@ -46,28 +49,32 @@ jest.mock('@react-navigation/native-stack', () => {
     };
 });
 
+// Authentication mock
 jest.mock('@/contexts/auth', () => ({
     useAuth: jest.fn(),
 }));
 
+// Library Settings mock 
 jest.mock('react-native/Libraries/Settings/Settings', () => ({
     get: jest.fn(),
     set: jest.fn(),
 }));
 
+// Async storage for the pdf as it is stored in FBcloud storage
 jest.mock('@react-native-async-storage/async-storage', () => ({
     setItem: jest.fn(),
     getItem: jest.fn(),
     removeItem: jest.fn(),
 }));
 
+// Firebase mock
 jest.mock('@/config/firebase', () => ({
     CollectionOf: jest.fn(),
     getDownloadURL: jest.fn(),
     callFunction: jest.fn()
 }));
 
-// Mock react-native-blob-util to prevent NativeEventEmitter errors
+// react-native-blob-util to prevent NativeEventEmitter errors
 jest.mock('react-native-blob-util', () => ({
     fs: {
         dirs: {
@@ -79,7 +86,7 @@ jest.mock('react-native-blob-util', () => ({
     })),
 }));
 
-// Mock Firebase Messaging to avoid NativeEventEmitter errors
+// Firebase Messaging to avoid NativeEventEmitter errors
 jest.mock('@react-native-firebase/messaging', () => ({
     __esModule: true,
     default: jest.fn(() => ({
@@ -103,11 +110,7 @@ jest.mock('expo-screen-orientation', () => ({
     removeOrientationChangeListener: jest.fn(),
 }));
 
-/* THIS IS THE PROBLEM
-jest.mock('react-native', () => ({
-    Vibration: { vibrate: jest.fn() }
-}));*/
-
+// Voice handler
 jest.mock('@react-native-voice/voice', () => ({
     start: jest.fn(),
     stop: jest.fn(),
@@ -115,24 +118,18 @@ jest.mock('@react-native-voice/voice', () => ({
     onSpeechError: jest.fn(),
 }));
 
-// Mocked data and utility functions
-const mockLectureData = {
-    data: {
-        nbOfPages: 5,
-    },
-}
-const mockParams = { courseNameString: 'testCourse', lectureIdString: 'testLectureId' };
 
+// 
 jest.mock('@/hooks/firebase/firestore', () => ({
     usePrefetchedDynamicDoc: jest.fn(),  // Note the double array to match [lectureDoc]
 }));
 
-// We need this
+// The expo core modules for the voice support
 jest.mock('expo-modules-core', () => ({
     NativeModulesProxy: {},
-    // Mock any other functions from expo-modules-core if needed
 }));
 
+// Useful for the prev-next button as they only contain icons
 jest.mock('@expo/vector-icons', () => {
     const React = require('react');
     return {
@@ -140,7 +137,20 @@ jest.mock('@expo/vector-icons', () => {
     };
 });
 
+// Mocked Lecture data
+// Note: this contains only the amount of information needed by the
+// rmctrl do not reuse it later if you require a full lectureDoc mock data
+const mockLectureData = {
+    data: {
+        nbOfPages: 5,
+    },
+}
+
 // Mock the AbstractRmtCtl component
+// Note: as we are mocking it this way, the lines in which we 
+//       put it argument, won't be convered.
+//       However, acting this way allow us to be able test idently the voice
+//       framework from the UI, apllying thus the MVVM model in a more consistent way.
 jest.mock('@/components/lectures/remotecontrol/abstractRmtCtl', () => {
     const TView = require('@/components/core/containers/TView').default;
     return {
@@ -188,5 +198,35 @@ describe('RemoteControlScreen Test Suite', () => {
         const { getByTestId } = render(<RemoteControlScreen />);
         //renderer.debug();
         expect(getByTestId('abstract-rmt-ctl')).toBeTruthy();
+    });
+
+    it('handles error when starting voice recording', async () => {
+        const mockError = new Error('Voice start error');
+        (Voice.start as jest.Mock).mockImplementationOnce(() => {
+            throw mockError;
+        });
+
+        console.log = jest.fn();
+
+        await act(async () => {
+            await startRecording();
+        });
+
+        expect(console.log).toHaveBeenCalledWith(mockError);
+    });
+
+    it('handles error when stopping voice recording', async () => {
+        const mockError = new Error('Voice stop error');
+        (Voice.stop as jest.Mock).mockImplementationOnce(() => {
+            throw mockError;
+        });
+
+        console.log = jest.fn();
+
+        await act(async () => {
+            await stopRecording();
+        });
+
+        expect(console.log).toHaveBeenCalledWith(mockError);
     });
 });
