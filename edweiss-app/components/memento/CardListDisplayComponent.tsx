d225e5426df98db68f3dcd9acefc2097e250cdeb
Tuan <tuan.nguyen@epfl.ch>
2024-11-19 19:30:31 +0100

    docs: add documentation comments for CardListDisplayComponent and its props

--- edweiss-app/components/memento/CardListDisplayComponent.tsx
+++ edweiss-app/components/memento/CardListDisplayComponent.tsx
@@ -1,3 +1,13 @@
+/**
+ * @file CardListDisplayComponent.tsx
+ * @description Visualize each card in a deck
+ * @author Tuan Dang Nguyen
+ */
+
+// ------------------------------------------------------------
+// --------------- Import Modules & Components ----------------
+// ------------------------------------------------------------
+
 import Memento from '@/model/memento';
 import { getStatusColor } from '@/utils/memento/utilsFunctions';
 import { BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
@@ -5,6 +15,19 @@ import React from 'react';
 import TTouchableOpacity from '../core/containers/TTouchableOpacity';
 import TText from '../core/TText';
 
+/**
+ * CardListDisplay
+ * 
+ * @param {Memento.Card} card - The card to display
+ * @param {boolean} isSelected - Whether the card is selected
+ * @param {Function} toggleSelection - Function to toggle card selection
+ * @param {Function} onLongPress - Function to handle long press
+ * @param {boolean} selectionMode - Whether the selection mode is active
+ * @param {React.Dispatch<React.SetStateAction<Memento.Card | undefined>>} setCardToDisplay - Function to set the card to display
+ * @param {React.RefObject<BottomSheetModalMethods>} modalRef - Reference to the modal
+ * 
+ * @returns {React.FC} CardListDisplay component 
+ */
 export const CardListDisplay: React.FC<{
     card: Memento.Card,
     isSelected: boolean,
