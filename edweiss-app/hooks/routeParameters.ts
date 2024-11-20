97a281130d8485f1dc661ac7550d15ce3ccbc3b8
Holo <yousseflaraki99@gmail.com>
2024-11-20 23:12:56 +0100

    docs: adds full documentation of the repository and type-safe routing

--- edweiss-app/hooks/routeParameters.ts
+++ edweiss-app/hooks/routeParameters.ts
@@ -1,23 +1,48 @@
 import { Href, router, useLocalSearchParams } from 'expo-router';
+import { useMemo } from 'react';
 
-export interface ApplicationRouteParameters<T> {
+/**
+ * The application route type signature alongside its path.
+ * 
+ * Use with {@link useRouteParameters} and {@link pushWithParameters}.
+ */
+export interface ApplicationRouteSignature<T> {
 	path: Href<string>
 }
 
-export function useStringParameters(): { [x in string]: string } {
-	return useLocalSearchParams();
-}
-
-export function useRouteParameters<T>(_: ApplicationRouteParameters<T>): T {
+/**
+ * Parses the route parameters with {@link ApplicationRouteSignature} to deduce the type of the parameters.
+ * 
+ * Use {@link pushWithParameters} to push the right arguments.
+ * 
+ * @returns The parsed parameters.
+ */
+export function useRouteParameters<T>(_: ApplicationRouteSignature<T>): T {
 	const { params } = useLocalSearchParams();
-	return JSON.parse(params as string) as T;
+
+	const parsed = useMemo(() => JSON.parse(params as string) as T, []);
+
+	return parsed;
 }
 
-export function pushWithParameters<T>(route: ApplicationRouteParameters<T>, params: T) {
+/**
+ * Pushes type-safe arguments to the given route.
+ * 
+ * @param signature The pushed route. It also gives type-safe constraints to the arguments.
+ * @param params Arguments to push
+ */
+export function pushWithParameters<T>(signature: ApplicationRouteSignature<T>, params: T) {
 	router.push({
-		pathname: route.path as any,
+		pathname: signature.path as any,
 		params: {
 			params: JSON.stringify(params)
 		}
 	});
+}
+
+/**
+ * @returns `useLocalSearchParams()` but only with string parameters returned.
+ */
+export function useStringParameters(): { [x in string]: string } {
+	return useLocalSearchParams();
 }
