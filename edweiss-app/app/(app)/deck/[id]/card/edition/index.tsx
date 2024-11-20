d56f56dbe650759d195aef660f61cfb8a6a3f80b
Holo <yousseflaraki99@gmail.com>
2024-11-20 18:22:19 +0100

    feat: decks are now handled as a repository

--- edweiss-app/app/(app)/deck/[id]/card/edition/index.tsx
+++ edweiss-app/app/(app)/deck/[id]/card/edition/index.tsx
@@ -18,11 +18,13 @@ import TScrollView from '@/components/core/containers/TScrollView';
 import TView from '@/components/core/containers/TView';
 import FancyButton from '@/components/input/FancyButton';
 import FancyTextInput from '@/components/input/FancyTextInput';
-import { callFunction, Collections } from '@/config/firebase';
-import { useDynamicDocs } from '@/hooks/firebase/firestore';
+import { callFunction } from '@/config/firebase';
+import { useRepositoryDocument } from '@/hooks/repository';
+import { useStringParameters } from '@/hooks/routeParameters';
 import Memento from '@/model/memento';
-import { router, useLocalSearchParams } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import React, { useState } from 'react';
+import { DecksRepository } from '../../../_layout';
 
 // ------------------------------------------------------------
 // ----------------- EditCardScreen Component -----------------
@@ -35,77 +37,92 @@ import React, { useState } from 'react';
  * @returns {ApplicationRoute} Screen to edit a card
  */
 const EditCardScreen: ApplicationRoute = () => {
-    const { deckId, prev_question, prev_answer, cardIndex } = useLocalSearchParams();
-    const [question, setQuestion] = useState(prev_question as string);
-    const [answer, setAnswer] = useState(prev_answer as string);
+	const { deckId, prev_question, prev_answer, cardIndex } = useStringParameters();
+	const [question, setQuestion] = useState(prev_question as string);
+	const [answer, setAnswer] = useState(prev_answer as string);
 
-    const decks = useDynamicDocs(Collections.deck);
-    const deck = decks?.find(d => d.id == deckId);
-    const cardIndexInt = cardIndex ? parseInt(cardIndex.toString()) : 0;
+	// const decks = useDynamicDocs(Collections.deck);
+	// const deck = decks?.find(d => d.id == deckId);
+	const [deck, handler] = useRepositoryDocument(deckId, DecksRepository);
+	if (deck == undefined)
+		return <Redirect href={'/'} />;
 
-    const card = deck?.data.cards[cardIndexInt];
+	const cardIndexInt = cardIndex ? parseInt(cardIndex.toString()) : 0;
 
-    // Update a card with a new question and answer
-    async function updateCard() {
+	const card = deck.data.cards[cardIndexInt];
 
-        const res = await callFunction(Memento.Functions.updateCard, { deckId: deckId as any, newCard: card, cardIndex: cardIndexInt });
+	// Update a card with a new question and answer
+	async function updateCard() {
+		if (deck == undefined)
+			return;
 
-        if (res.status == 1) {
-            console.log(`OKAY, card updated with index ${cardIndexInt}`);
-            router.back();
-        }
+		const newCards = deck.data.cards;
+		newCards[cardIndexInt] = card;
 
-    }
+		handler.modifyDocument(deckId, {
+			cards: newCards
+		}, (deckId) => {
+			callFunction(Memento.Functions.updateCard, { deckId, newCard: card, cardIndex: cardIndexInt });
+		});
 
-    return (
-        <>
-            <RouteHeader title='Edit card' />
 
-            <TScrollView>
-                {/* <TextInput value={deckName} onChangeText={n => setDeckName(n)} placeholder='Deck name' placeholderTextColor={'#555'} style={{ backgroundColor: Colors.dark.crust, borderColor: Colors.dark.blue, borderWidth: 1, padding: 8, paddingHorizontal: 16, margin: 16, marginBottom: 0, color: 'white', borderRadius: 14, fontFamily: "Inter" }}>
+		// console.log(`OKAY, card updated with index ${cardIndexInt}`);
+		// if (res.status == 1) {
+		// }
+
+		router.back();
+
+	}
+
+	return (
+		<>
+			<RouteHeader title='Edit card' />
+
+			<TScrollView>
+				{/* <TextInput value={deckName} onChangeText={n => setDeckName(n)} placeholder='Deck name' placeholderTextColor={'#555'} style={{ backgroundColor: Colors.dark.crust, borderColor: Colors.dark.blue, borderWidth: 1, padding: 8, paddingHorizontal: 16, margin: 16, marginBottom: 0, color: 'white', borderRadius: 14, fontFamily: "Inter" }}>
 
 				</TextInput> */}
-                <TView testID='questionInput' m='md' borderColor='crust' radius='lg'>
-                    <FancyTextInput
-                        value={question}
-                        onChangeText={n => setQuestion(n)}
-                        placeholder='My amazing question'
-                        icon='help-sharp'
-                        label='Question'
-                        //error='Invalid deck name'
-                        multiline
-                        numberOfLines={3}
-                    />
-                </TView>
+				<TView testID='questionInput' m='md' borderColor='crust' radius='lg'>
+					<FancyTextInput
+						value={question}
+						onChangeText={n => setQuestion(n)}
+						placeholder='My amazing question'
+						icon='help-sharp'
+						label='Question'
+						//error='Invalid deck name'
+						multiline
+						numberOfLines={3}
+					/>
+				</TView>
 
-                <TView m='md' mt={2} borderColor='crust' radius='lg'>
-                    <FancyTextInput
-                        value={answer}
-                        onChangeText={n => setAnswer(n)}
-                        placeholder='My amazing answer'
-                        icon='bulb-outline'
-                        label='Answer'
-                        //error='Invalid deck name'
-                        multiline
-                        numberOfLines={3}
-                    />
-                </TView>
+				<TView m='md' mt={2} borderColor='crust' radius='lg'>
+					<FancyTextInput
+						value={answer}
+						onChangeText={n => setAnswer(n)}
+						placeholder='My amazing answer'
+						icon='bulb-outline'
+						label='Answer'
+						//error='Invalid deck name'
+						multiline
+						numberOfLines={3}
+					/>
+				</TView>
 
-                <FancyButton testID='updateCardButton' onPress={() => updateQuestionAnswerCard(deckId as any, cardIndexInt, updateCard, question, answer, card)} backgroundColor='blue' mt={'md'} mb={'sm'} icon='logo-firebase' mx={300} >
-                    Done!
-                </FancyButton>
+				<FancyButton testID='updateCardButton' onPress={() => updateQuestionAnswerCard(deckId as any, cardIndexInt, updateCard, question, answer, card)} backgroundColor='blue' mt={'md'} mb={'sm'} icon='logo-firebase' mx={300} >
+					Done!
+				</FancyButton>
 
-            </TScrollView >
-        </>
-    );
+			</TScrollView >
+		</>
+	);
 };
 
 export default EditCardScreen;
 
 function updateQuestionAnswerCard(deckId: string, cardIndex: number, onPress: (deckId: any, newCard: Memento.Card, cardIndex: number) => void, newQuestion: string, newAnswer: string, card?: Memento.Card) {
-    if (card) {
-        card.answer = newAnswer;
-        card.question = newQuestion;
-        onPress(deckId, card, cardIndex);
-    }
+	if (card) {
+		card.answer = newAnswer;
+		card.question = newQuestion;
+		onPress(deckId, card, cardIndex);
+	}
 }
