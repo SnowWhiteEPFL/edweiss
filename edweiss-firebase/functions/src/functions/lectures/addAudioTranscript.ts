8261edb441d788bcd441e5e1fe8ca56690509253
Adamm Alaoui <adamm.2.alaoui@gmail.com>
2024-11-28 20:20:51 +0100

    refactor: clean the addaudiotranscript cloud function

--- edweiss-firebase/functions/src/functions/lectures/addAudioTranscript.ts
+++ edweiss-firebase/functions/src/functions/lectures/addAudioTranscript.ts
@@ -1,77 +1,8 @@
-// /**
-//  * @file addAudioTranscript.ts
-//  * @description Cloud function for adding audio transcripts to lecture documents in the edweiss app
-//  * @author Adamm Alaoui & Youssef Laraki
-//  */
-
-// // ------------------------------------------------------------
-// // --------------- Import Modules & Components ----------------
-// // ------------------------------------------------------------
-
-// import LectureDisplay from 'model/lectures/lectureDoc';
-// import { onSanitizedCall } from 'utils/firebase';
-// import { CollectionOf, getDocumentAndRef } from 'utils/firestore';
-// import { Predicate, assert } from 'utils/sanitizer';
-// import { fail, ok } from 'utils/status';
-// import Functions = LectureDisplay.Functions;
-
-// // Types
-// type Lecture = LectureDisplay.Lecture;
-
-// // ------------------------------------------------------------
-// // -----------  Add Audio Transcript Cloud Function   ---------
-// // ------------------------------------------------------------
-
-// export const addAudioTranscript = onSanitizedCall(Functions.addAudioTranscript, {
-// 	courseId: Predicate.isNonEmptyString,
-// 	lectureId: Predicate.isNonEmptyString,
-// 	transcription: Predicate.isNonEmptyString,
-// 	pageNumber: Predicate.isStrictlyPositive,
-// }, async (userId, args) => {
-// 	//const course = await getRequiredDocument(CollectionOf<Course>(`courses`), args.courseId, fail("course_not_found"));
-// 	const [lecture, lectureRef] = await getDocumentAndRef(CollectionOf<Lecture>(`courses/${args.courseId}/lectures`), args.lectureId);
-
-// 	if (lecture == undefined)
-// 		return fail("error_firebase");
-
-// 	assert(args.pageNumber > lecture.nbOfPages);
-
-// 	const pageKey = `audioTranscript.${args.pageNumber}`;
-
-// const correctedTranscript = await prompt({
-// 	task: `
-// 			This is a "${course.name}" lecture at prestigious university EPFL.
-// 			Description of the course: ${course.description}.
-
-// 			You are an audio transcript correcting AI.
-// 			Correct the following text captured by the microphone during the lecture.
-// 		`,
-// 	content: args.transcription,
-// 	fallback: args.transcription
-// });
-
-// 	const correctedTranscript = args.transcription;
-
-// 	if (!lecture.audioTranscript || !lecture.audioTranscript[args.pageNumber]) {
-// 		try {
-// 			await lectureRef.update({
-// 				[pageKey]: correctedTranscript
-// 			});
-// 		} catch (error) {
-// 			return fail('error_firebase');
-// 		}
-// 	} else if (args.transcription) {
-// 		try {
-// 			await lectureRef.update({
-// 				[pageKey]: lecture.audioTranscript[args.pageNumber] + correctedTranscript
-// 			});
-// 		} catch (error) {
-// 			return fail('error_firebase');
-// 		}
-// 	}
-
-// 	return ok('successfully_added');
-// });
+/**
+ * @file addAudioTranscript.ts
+ * @description Cloud function for adding audio transcripts to lecture documents in the edweiss app
+ * @author Adamm Alaoui & Youssef Laraki
+ */
 
 // ------------------------------------------------------------
 // --------------- Import Modules & Components ----------------
