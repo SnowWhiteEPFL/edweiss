d145c5be9285317692ed0d7294006e75458a50fd
Holo <yousseflaraki99@gmail.com>
2024-11-28 23:07:45 +0100

    chore: adds code example in community tab

--- edweiss-app/app/(app)/(tabs)/community.tsx
+++ edweiss-app/app/(app)/(tabs)/community.tsx
@@ -14,14 +14,50 @@ const CommunityTab: ApplicationRoute = () => {
 	 * This is only a demo editor, don't use this as an example of
 	 * how to render rich texts.
 	 */
-	const [richText, setRichText] = useState(`
-# Intro to Rich Text
+	const [richText, setRichText] = useState(`# Intro to Rich Text
 ### Titles
 As you can see in the editor above, use # to define titles.
 ### Math expressions
-The most beautiful formula: $e^{i\pi} + 1 = 0$
+The most beautiful formula: $e^{i\\pi} + 1 = 0$
 The actual most beautiful formula: $$\\int\\limits f(t)g(t) dt = \\int\\limits f(t) dt \\cdot \\int\\limits g(t) dt$$
-	`.trim().replaceAll("\t", ""));
+### Code
+${"```typescript"}
+const a: number = 15;
+const str = "Hello, world!";
+console.log(a + b);
+
+/*
+Multi line comments! Amazing
+*/
+function add(a, b) {
+	return a + b;
+}
+${"```"}
+${"```python"}
+# Comments with LaTeX,
+# $S = \\sum x_i$
+def sum(array):
+	return array.sum(type='fast')
+${"```"}
+${"```java"}
+package fox.holo;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
+${"```"}
+${"```rust"}
+fn add(a: i32, b: i32) -> i32 {
+	a + b
+}
+${"```"}
+I think you can see now how powerful it is
+to be able to represent all of this using
+only text. It can be fetched from Firebase,
+genrated by AI etc.
+`);
 
 	return (
 		<>
