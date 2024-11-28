16773418a424be94827350456c5851fdd3f712d4
Holo <yousseflaraki99@gmail.com>
2024-11-28 02:26:03 +0100

    feat: full implementation of rich text rendering using RNWebView and MathJax

--- edweiss-app/components/core/rich-text/RichText.tsx
+++ edweiss-app/components/core/rich-text/RichText.tsx
@@ -1,62 +1,118 @@
-import TView from '@/components/core/containers/TView';
+import Colors, { Color } from '@/constants/Colors';
 import ReactComponent from '@/constants/Component';
-import React from 'react';
-import { WebView } from 'react-native-webview';
-// import MathJax from 'react-native-mathjax';
+import { MarginProps, PaddingProps, PredefinedSizes, Size, computeMargins, computeSize, computeSizeOpt, paddingSizes } from '@/constants/Sizes';
+import useTheme from '@/hooks/theme/useTheme';
+import { useOptionalColor } from '@/hooks/theme/useThemeColor';
+import React, { useMemo } from 'react';
+import { ViewStyle } from 'react-native';
+import MathJax from 'react-native-mathjax';
+import sanitizeHtml from 'sanitize-html';
 
-interface RichTextProps {
-	children: string
+type RichTextProps = PaddingProps & MarginProps & {
+	backgroundColor?: Color,
+	color?: Color,
+	children: string,
+	size?: Size,
+	font?: "Inter" | "Times New Roman"
 }
 
-const RichText: ReactComponent<RichTextProps> = (props) => {
-	// const test = usePromise(async () => {
-	// 	return await tex2svg(props.children, {
-	// 		showConsole: true
-	// 	});
-	// });
+const headerSizes = [
+	32,
+	28,
+	24,
+	20,
+	18,
+	16
+]
 
-	// console.log(test);
+const richTextSizes: PredefinedSizes = {
+	xs: 0.8,
+	sm: 0.9,
+	md: 1.0,
+	lg: 1.2,
+	xl: 1.4
+}
+
+function headerLevel(line: string, level: number) {
+	return `<div style="font-size: ${headerSizes[level - 1]}px; font-weight: bold">${line.substring(level + 1)}</div>`;
+}
+
+export function richTextToHTML(rawText: string): string {
+	const text = sanitizeHtml(rawText, {
+		allowedTags: [] // no tags allowed so no injection attacks are permitted
+	}).trim();
+
+	const lines = text.split("\n");
+
+	let res = "";
+
+	main:
+	for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+		if (lineIndex != 0) {
+			res += '<br/>';
+		}
+
+		const line = lines[lineIndex].trim();
+
+		for (let headerCheckIndex = 0; headerCheckIndex < 6; headerCheckIndex++) {
+			if (line.startsWith("#".repeat(headerCheckIndex + 1) + " ")) {
+				res += headerLevel(line, headerCheckIndex + 1);
+				continue main;
+			}
+		}
+
+		res += line;
+	}
+
+	return res;
+}
+
+const RichText: ReactComponent<RichTextProps> = ({ size = "md", ...props }) => {
+	const text = useMemo(() => richTextToHTML(props.children), [props.children]);
+
+	const theme = useTheme();
+	const computedBackgroundColor = useOptionalColor(props.backgroundColor);
+	const computedColor = useOptionalColor(props.color);
 
 	return (
-		<TView>
-			<WebView
-				style={{
-					width: '100%',
-					height: 400
-				}}
-				source={{ uri: 'https://expo.dev' }}
-			/>
-			{/* <MathJax
-				// HTML content with MathJax support
-				html={'$\sum_{i=0}^n i^2 = \frac{(n^2+n)(2n+1)}{6}$<br><p>This is an equation</p>'}
-				// MathJax config option
-				mathJaxOptions={{
-					messageStyle: 'none',
-					extensions: ['tex2jax.js'],
-					jax: ['input/TeX', 'output/HTML-CSS'],
-					tex2jax: {
-						inlineMath: [['$', '$'], ['\\(', '\\)']],
-						displayMath: [['$$', '$$'], ['\\[', '\\]']],
-						processEscapes: true,
-					},
-					TeX: {
-						extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
+		<MathJax
+			style={{
+				backgroundColor: computedBackgroundColor ?? 'transparent',
+				...computeMargins(props)
+			} satisfies ViewStyle}
+			html={`
+				<style>
+					@font-face {
+						font-family: Inter;
+						src: url('file:///android_asset/fonts/Inter.ttf') format('truetype')
 					}
-				}}
-			// {...WebView props}
-			/> */}
-			{/* <MathText
-				content={`
-            $$E = mc^2$$
-            $2^{1+2}$
-            Here is an inline equation: $a^2 + b^2 = c^2$
-            <p>Additional HTML content can go here.</p>
-          `}
-				textSize={20}
-				textColor={"#333"}
-				style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-			/> */}
-		</TView>
+					body {
+						width: 100%;
+						height: 100%;
+
+						margin: 0 !important;
+ 						padding: ${computeSizeOpt(props.p, paddingSizes) ?? 0} !important;
+						padding-top: ${computeSizeOpt(props.pt, paddingSizes) ?? 0} !important;
+						padding-bottom: ${computeSizeOpt(props.pb, paddingSizes) ?? 0} !important;
+						padding-left: ${computeSizeOpt(props.pl, paddingSizes) ?? 0} !important;
+						padding-right: ${computeSizeOpt(props.pr, paddingSizes) ?? 0} !important;
+						
+						font-family: ${props.font ?? "Inter"};
+						color: ${computedColor ?? Colors[theme].text};
+						font-size: ${16 * computeSize(size, richTextSizes)}px;
+						line-height: 1.6;
+					}
+				</style>
+				<body>
+					${text}
+				</body>
+			`}
+			scrollEnabled={false}
+			allowBounce={false}
+			alwaysBounceHorizontal={false}
+			alwaysBounceVertical={false}
+			bounces={false}
+		/>
 	);
 };
 
