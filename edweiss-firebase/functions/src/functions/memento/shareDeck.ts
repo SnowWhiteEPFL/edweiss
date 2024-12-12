d63f6727a777f26b7a86715cd7b98013e50bd95a
Tuan <tuan.nguyen@epfl.ch>
2024-12-13 00:35:46 +0100

    feat: enhance shareDeck function to prevent card duplication and manage ownerID updates

--- edweiss-firebase/functions/src/functions/memento/shareDeck.ts
+++ edweiss-firebase/functions/src/functions/memento/shareDeck.ts
@@ -12,24 +12,34 @@ export const shareDeck = onSanitizedCall(Memento.Functions.shareDeck, {
     const my_deckCollection = CollectionOf<Memento.Deck>(`users/${userId}/courses/${args.courseId}/decks`);
     const other_deckCollection = CollectionOf<Memento.Deck>(`users/${args.other_user}/courses/${args.courseId}/decks`);
 
-    const shared_deckDoc = await my_deckCollection.doc(args.deckId).get();
-    const shared_deck = shared_deckDoc.data();
+    const my_deckDoc = await my_deckCollection.doc(args.deckId).get();
+    const my_deck = my_deckDoc.data();
 
-    if (!shared_deck) {
+    if (!my_deck) {
         return fail("deck_not_found");
     }
 
     // If there is a deck with the same name and the same ownerID[0] == userId, if yes, only update the cards instance
-    const other_deckDoc = await other_deckCollection.where("name", "==", shared_deck.name).where("ownerID", "array-contains", args.other_user).get();
+    const other_deckDoc = await other_deckCollection.where("name", "==", my_deck.name).where("ownerID", "array-contains", userId).get();
     if (other_deckDoc.size > 0) {
         const other_deck = other_deckDoc.docs[0].data();
         const other_cards = other_deck.cards;
-        const updated_cards = [...other_cards, ...shared_deck.cards];
+
+        // If the other deck already has the same cards untouched (== didn't change question || answer) then only add new cards
+        // This allows the user to share the deck with the same user multiple times without duplicating the cards
+        // and also other to have 2 same questions but might have different answers
+        const cards_to_add = my_deck.cards.filter(card => !other_cards.some(other_card => other_card.question === card.question && other_card.ownerID === card.ownerID));
+        const updated_cards = [...other_cards, ...cards_to_add];
+
         await other_deckCollection.doc(other_deckDoc.docs[0].id).update({ cards: updated_cards });
+
         return ok({ id: other_deckDoc.docs[0].id });
     }
 
-    const shared_deck_with_added_owner = { ...shared_deck, ownerID: [...shared_deck.ownerID, args.other_user] };
+    // Add other_user to the ownerID array
+    await my_deckCollection.doc(args.deckId).update({ ownerID: [...my_deck.ownerID, args.other_user] });
+
+    const shared_deck_with_added_owner = { ...my_deck, ownerID: [...my_deck.ownerID, args.other_user] };
 
     const res = await other_deckCollection.add(shared_deck_with_added_owner);
 
