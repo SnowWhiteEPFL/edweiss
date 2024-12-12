b8abe1bfb8ef9859ab97c686d8a4a4a2e05c1ef2
Holo <yousseflaraki99@gmail.com>
2024-12-12 19:10:29 +0100

    fix: rich text rendering

--- edweiss-app/components/core/rich-text/WebViewMathJaxWrapper.tsx
+++ edweiss-app/components/core/rich-text/WebViewMathJaxWrapper.tsx
@@ -30,12 +30,14 @@ const WebViewMathJaxWrapper: ReactComponent<WebViewJaxWrapperProps> = (props) =>
 	const html = useMemo(() => `
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		${props.disableMathJax ? `
+			<div id="jax-content">${props.source}</div>
 			<script type="text/javascript">
+
+				// window.ReactNativeWebView.postMessage(String(document.documentElement.scrollHeight));
 				setTimeout(() => {
 					window.ReactNativeWebView.postMessage(String(document.documentElement.scrollHeight));
 				}, 48);
 			</script>
-			<div id="jax-content">${props.source}</div>
 		` : `
 			<script type="text/x-mathjax-config">
 				MathJax.Hub.Config(${mathjaxConfigPayload});
@@ -52,6 +54,10 @@ const WebViewMathJaxWrapper: ReactComponent<WebViewJaxWrapperProps> = (props) =>
 	return (
 		<View style={[{ height }, props.style]}>
 			<WebView
+				textInteractionEnabled={false}
+				scrollEnabled={false}
+				overScrollMode='never'
+				showsVerticalScrollIndicator={false}
 				source={{ html }}
 				onMessage={message => setHeight(Number(message.nativeEvent.data))}
 				cacheEnabled
