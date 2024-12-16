6bd91875da5f0796765ed405a01458cc9e316837
Dinant Florian <florian.dinant@epfl.ch>
2024-12-16 14:45:49 +0100

    refractor: use fetch instead of axios and update tests accordingly

--- edweiss-app/components/core/SVGImage.tsx
+++ edweiss-app/components/core/SVGImage.tsx
@@ -1,6 +1,5 @@
 import t from '@/config/i18config';
 import ReactComponent from '@/constants/Component';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
 import { SvgProps, SvgXml } from 'react-native-svg';
@@ -19,14 +18,15 @@ export type SVGImageProps = { uri: string } & SvgProps & AdditionalProps;
  */
 const SVGImage: ReactComponent<SVGImageProps> = ({ uri, width, height, color, title, onError, onLoad, fallback }) => {
 
-    const [svgData, setSvgData] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [svgData, setSvgData] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const fetchSvg = async () => {
             try {
-                const response = await axios.get(uri);
-                setSvgData(response.data);
+                const response = await fetch(uri);
+                const svgText = await response.text();
+                setSvgData(svgText);
                 setLoading(false);
             } catch (error) {
                 console.error('Error fetching SVG: ', error);
